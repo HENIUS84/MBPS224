b9219c9d77b982fd7d8a794a09ef33494965994f
Paweł Witak <hensoft.pawelwitak@gmail.com>
2020-04-22 14:20:40 +0200

    Cosmetic improvements in UTs project

--- firmware/unit_tests/main.cpp
+++ firmware/unit_tests/main.cpp
@@ -1,7 +1,33 @@
+/**
+ *******************************************************************************
+ * @file     main.cpp
+ * @author   HENIUS (Pawe³ Witak)
+ * @version  1.0.0
+ * @date     22-04-2020
+ * @brief    Main program
+ *******************************************************************************
+ *
+ * <h2><center>COPYRIGHT 2020 HENIUS</center></h2>
+ */
+
+/* Include section -----------------------------------------------------------*/
+
+// --->System files
+
 #include <gtest/gtest.h>
 
+/* Function section ----------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/**
+* @brief    Main function
+* @param    None
+* @retval   Exit code
+*/
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
+/******************* (C) COPYRIGHT 2020 HENIUS *************** END OF FILE ****/
--- firmware/unit_tests/tests/common/utils/utils_test.cpp
+++ firmware/unit_tests/tests/common/utils/utils_test.cpp
@@ -1,13 +1,37 @@
+/**
+ *******************************************************************************
+ * @file     utils_test.h
+ * @author   HENIUS (Pawe³ Witak)
+ * @version  1.0.0
+ * @date     22-04-2020
+ * @brief    Tests of file utils.c
+ *******************************************************************************
+ *
+ * <h2><center>COPYRIGHT 2020 HENIUS</center></h2>
+ */
+
+/* Include section -----------------------------------------------------------*/
+
+// --->User files
+
 #include "Utils.c"
 
 #define TEST_CLASS_NAME UtilsTest
 #include "base_test.h"
 
+/* Declaration section -------------------------------------------------------*/
+
 TEST_CLASS()
 {
 };
+
+/* Function section ----------------------------------------------------------*/
+
+/* Tests section -------------------------------------------------------------*/
 
 UNIT_TEST(AsciiHexToByteTest)
 {
 	EXPECT_EQ(AsciiHexToByte((uint8_t *)"AA"), 0xAA);
 }
+
+/******************* (C) COPYRIGHT 2020 HENIUS *************** END OF FILE ****/
